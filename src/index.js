bfcb0d81d794c0c2a8b8572cee9305326227c138
And-Naz <nazaryan-ando@mail.ru>
2022-02-03 18:22:04 +0400

    Fixed issue related unavailable Promise for IE and import objectFitPolyfill staticly

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import "./utils/ObjectFitPolyfill"
+import "objectFitPolyfill" /* Can't do dynamic import becouse that based on Promise and in IE 11 Promise isn't available */
 
 ReactDOM.render(
 	<React.StrictMode>
--- src/utils/ObjectFitPolyfill.js
+++
@@ -1,9 +0,0 @@
-import { lazy } from "react"
-import GetBrowserInfo from "../helpers/GetBrowserInfo"
-if (GetBrowserInfo()?.isIE) {
-    function loadPolyFill() {
-        lazy(() => import("objectFitPolyfill").then(polyfill => (console.log("importing Polyfill for IE"), polyfill)))
-    }
-    document.addEventListener("DOMContentLoaded", loadPolyFill)
-    
-}
