4db17f29c4955eb950cb4523e15c6d78f4a5f0b4
Hasmik Margaryan <hasmikmargaryan277@gmail.com>
2022-02-03 23:49:26 +0400

    header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,7 @@ import {NavLink} from "react-router-dom";
 
 const Header = () => (
     <header className='header'>
-            <div className='container'>
+            <div className='containe'>
                     <NavLink className='navLink' to=''>Home</NavLink>
                     <NavLink className='navLink' to='gameList'>GameList</NavLink>
                     <NavLink className='navLink' to='score'>Score</NavLink>
