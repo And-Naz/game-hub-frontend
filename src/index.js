440930a01c84724855ec8ac8e94871dde50510e6
And-Naz <nazaryan-ando@mail.ru>
2022-02-04 15:19:00 +0400

    Removed objectFitPolyfill from dependencies, add core-js/stable polyfill to provide promises, iterators and more for IE8+, Enhancement Image component(separated two components default and polyfilled), include in dependencies object-fit-images

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+import "./utils/polyfills"
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import "objectFitPolyfill" /* Can't do dynamic import becouse that based on Promise and in IE 11 Promise isn't available */
 
 ReactDOM.render(
 	<React.StrictMode>
