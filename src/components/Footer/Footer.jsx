8327901261d0c25331ab6b2ab1ef40e83090584b
Aram Baghdasaryan <arambag.js@gmail.com>
2022-02-05 16:21:03 +0400

    Add some styles to Partners Carousel

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,45 +10,41 @@ const Footer = () => {
 
     return (
         <>
-        <Partners />
-        <div className="footerWrapper">
+            <Partners/>
+            <div className="footerWrapper">
 
 
-        <div className="footerWrapper">
-            <section className="feedback-section">
+                <div className="footerWrapper">
+                    <section className="feedback-section">
 
-                {/*<span className="footer-icon"><i ref={"*"}/></span>*/}
-                <h2>Connect socially with "AVAHas Games"</h2>
-                <div className="footerSocialCardsContainer">
-                    <div className={cardClasses[0]}>
-                        <i>F icon</i>
-                        <p>Link on Facebook</p>
-                    </div>
-                    <div className={cardClasses[1]}>
-                        <i>T icon</i>
-                        <p>Follow on Twitter</p>
-                    </div>
-                    <div className={cardClasses[2]}>
-                        <i>G icon</i>
-                        <p>Follow on Google+</p>
-                    </div>
-                    <div className={cardClasses[3]}>
-                        <i>Y icon</i>
-                        <p>Watch on Youtube</p>
-                    </div>
+                        {/*<span className="footer-icon"><i ref={"*"}/></span>*/}
+                        <h2>Connect socially with "AVAHas Games"</h2>
+                        <div className="footerSocialCardsContainer">
+                            <div className={cardClasses[0]}>
+                                <i>F icon</i>
+                                <p>Link on Facebook</p>
+                            </div>
+                            <div className={cardClasses[1]}>
+                                <i>T icon</i>
+                                <p>Follow on Twitter</p>
+                            </div>
+                            <div className={cardClasses[2]}>
+                                <i>G icon</i>
+                                <p>Follow on Google+</p>
+                            </div>
+                            <div className={cardClasses[3]}>
+                                <i>Y icon</i>
+                                <p>Watch on Youtube</p>
+                            </div>
+                        </div>
+
+                        <div className="footerCopyright">
+                            <strong> AVAHas Games © 2022. All rights reserved</strong>
+                        </div>
+
+                    </section>
                 </div>
-
-                    <div className="footerCopyright">
-                        <strong> AVAHas Games © 2022. All rights reserved</strong>
-                    </div>
-
-            </section>
-            {/*<div>*/}
-            {/*    <TestDiv/>*/}
-            {/*</div>*/}
-
-        </div>
-        </div>
+            </div>
         </>
     )
 }
--- src/components/Footer/PartnersSlider/PartnersSlider.jsx
+++ src/components/Footer/PartnersSlider/PartnersSlider.jsx
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "./PartnerSlider.css"
 
 const settings = {
             dots: true,
@@ -41,54 +42,54 @@ const settings = {
 export default class Partners extends Component {
     render() {
         return (
-            <div >
+            <div className='images-container'>
                 <h2> PARTNERS </h2>
                 <Slider {...settings}>
-                    <div className="firstPartner">
-                        <h3>1</h3>
-                        <img width="250" height="200"
+                    <div className="partner-image">
+                        {/*<h3>1</h3>*/}
+                        <img width="250" height="200" alt='some Image'
                              src="https://wp.nkdev.info/youplay/wp-content/uploads/2015/10/partner-logo-1-500x333.png"
                         />
                     </div>
-                    <div className="firstPartner">
-                        <p>2</p>
-                        <img width="250" height="200"
+                    <div className="partner-image">
+                        {/*<h3>2</h3>*/}
+                        <img width="250" height="200" alt='some Image'
                              src="https://wp.nkdev.info/youplay/wp-content/uploads/2015/10/partner-logo-1-500x333.png"
                         />
                     </div>
-                    <div className="firstPartner">
-                        <h3>3</h3>
-                        <img width="250" height="200"
+                    <div className="partner-image">
+                        {/*<h3>3</h3>*/}
+                        <img width="250" height="200" alt='someImage'
                              src="https://wp.nkdev.info/youplay/wp-content/uploads/2015/10/partner-logo-1-500x333.png"
                         />
                     </div>
-                    <div className="firstPartner">
-                        <h3>4</h3>
-                        <img width="250" height="200"
+                    <div className="partner-image">
+                        {/*<h3>4</h3>*/}
+                        <img width="250" height="200" alt='someImage'
                              src="https://wp.nkdev.info/youplay/wp-content/uploads/2015/10/partner-logo-1-500x333.png"
                         />
                     </div>
-                    <div className="firstPartner">
-                        <h3>5</h3>
-                        <img width="250" height="200"
+                    <div className="partner-image">
+                        {/*<h3>5</h3>*/}
+                        <img width="250" height="200" alt='someImage'
                              src="https://wp.nkdev.info/youplay/wp-content/uploads/2015/10/partner-logo-1-500x333.png"
                         />
                     </div>
-                    <div className="firstPartner">
-                        <h3>6</h3>
-                        <img width="250" height="200"
+                    <div className="partner-image">
+                        {/*<h3>6</h3>*/}
+                        <img width="250" height="200" alt='someImage'
                              src="https://wp.nkdev.info/youplay/wp-content/uploads/2015/10/partner-logo-1-500x333.png"
                         />
                     </div>
-                    <div className="firstPartner">
-                        <h3>7</h3>
-                        <img width="250" height="200"
+                    <div className="partner-image">
+                        {/*<h3>7</h3>*/}
+                        <img width="250" height="200" alt='someImage'
                              src="https://wp.nkdev.info/youplay/wp-content/uploads/2015/10/partner-logo-1-500x333.png"
                         />
                     </div>
-                    <div className="firstPartner">
-                        <h3>8</h3>
-                        <img width="250" height="200"
+                    <div className="partner-image">
+                        {/*<h3>8</h3>*/}
+                        <img width="250" height="200" alt='someImage'
                              src="https://wp.nkdev.info/youplay/wp-content/uploads/2015/10/partner-logo-1-500x333.png"
                         />
                     </div>
