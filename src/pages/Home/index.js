ea0ff3082710abcbba0afeffd442a6805819cb95
Hasmik Margaryan <hasmikmargaryan277@gmail.com>
2022-02-06 20:26:48 +0400

    Was updated Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,30 +1,22 @@
 import Image from "../../components/ui/Image";
 import Button from "../../components/ui/Button";
-import ImageSrc from "../../assets/images/1400x775.jpg"
 import './home.css'
 
 function Home() {
 	return (
-		<section className='content-wrap'>
-			<div className='youplay-banner-parallax youplay-banner youplay-banner-id-1 mid banner-top'>
-				<div className='image'>
-					<div className="jarallax-container-2">
-						<Image className='jarallax-img' src={ImageSrc} alt="home" />
-					</div>
-				</div>
-				<div className='info'>
-					<div className='container'>
-						<h2>
-							Clan War:
-							<br />
-							Global Esports Cup
-						</h2>
-						<p>
-							<strong>Virtus PRO</strong> vs <strong>Team Secret</strong>
-						</p>
-						<Button children='Learn More' />
-					</div>
-				</div>
+		<section>
+			<div className='img-container'>
+				<Image className='jarallax-img' src="https://wp.nkdev.info/youplay/wp-content/uploads/2015/06/banner-bg.jpg" alt="home" />
+				{/*<div className='layer'></div>*/}
+			</div>
+			<div className='info-container'>
+				<h1 className='info__header'>
+					Clan War:
+					<br />
+					Global Esports Cup
+				</h1>
+				<p className='info__paragraph'><strong>Virtus PRO</strong> vs <strong>Team Secret</strong></p>
+				<Button children='Learn More' type='button' buttonSize='btn-large' />
 			</div>
 		</section>
 	);
