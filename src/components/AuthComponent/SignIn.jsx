00c50b28afcba8779c7ccbb75df4be454102cb3b
Hasmik Margaryan <hasmikmargaryan277@gmail.com>
2022-02-06 19:07:54 +0400

    Was updated SignIn component

--- src/components/AuthComponent/SignIn.jsx
+++ src/components/AuthComponent/SignIn.jsx
@@ -1,5 +1,26 @@
+import Button from "../ui/Button";
+import './style.css'
+
 function SignIn() {
-	return <div></div>;
+	return (
+		<div className='navbar-login-form'>
+			<form action="#" method='post'>
+				<label htmlFor='userName'>Username:</label>
+				<div><input className="login-form-inp" type="text" id='userName' name="userName" /></div>
+				<label htmlFor='password'>Password:</label>
+				<div><input className="login-form-inp" type="password" id='password' name="password" /></div>
+
+				<div className='checkbox'>
+					<input className="login-form-checkbox" type="checkbox" id='checkbox' name="checkbox"/>
+					<label htmlFor='checkbox'>Remember Me</label>
+				</div>
+				<Button type='submit' buttonSize='btn-small' children='Log In' />
+				<div className='lostPwd'>
+					<a className="lostPwd__link"  href="#" title="Forgot password">Forgot password?</a>
+				</div>
+			</form>
+		</div>
+	)
 }
 
 export default SignIn;
--- src/components/PopupButton/index.jsx
+++ src/components/PopupButton/index.jsx
@@ -1,6 +1,7 @@
 import { useState, forwardRef, useCallback } from "react";
 import Button from "../ui/Button";
 import "./style.css"
+import SignIn from "../AuthComponent/SignIn";
 const PopupButton = forwardRef(function ({ children }, ref) {
 	const [showPopup, setShowPopup] = useState(false)
 	const onClickBtn = useCallback(() => { setShowPopup(prev => !prev) }, [])
@@ -18,23 +19,7 @@ const PopupButton = forwardRef(function ({ children }, ref) {
 			{
 				showPopup &&
 				<div className="popup-btn__popup-element">
-					<div className='navbar-login-form'>
-						<form action="#" method='post'>
-							<p> Username:</p>
-							<div><input className="login-form-inp" type="text" name="log" /></div>
-							<p> Password:</p>
-							<div><input className="login-form-inp" type="password" name="psw" /></div>
-							<br />
-							<div className='checkbox'>
-								<input className="login-form-checkbox" type="checkbox" name="rememberme" />
-								<label>Remember Me</label>
-							</div>
-							<Button buttonSize='btn-small' children='Log In' />
-							<div className="lostPwd">
-								<a href="#" title="Password Lost and Found">Lost password?</a>
-							</div>
-						</form>
-					</div>
+					<SignIn />
 				</div>
 			}
 		</button>
